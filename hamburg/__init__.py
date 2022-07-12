5f6486c942cc418a0b912c8851f08a01c444ba5c
Klaas Schoute <klaas_schoute@hotmail.com>
2022-07-12 19:45:26 +0200

    Add pre-commit (#1)
    * Add pre-commit checks
    
    * Update code follow pre-commit checks

--- hamburg/__init__.py
+++ hamburg/__init__.py
@@ -1,10 +1,6 @@
 """Asynchronous Python client providing Urban Data information of Hamburg."""
 
-from .exceptions import (
-    UDPConnectionError,
-    UDPResultsError,
-    UDPError,
-)
+from .exceptions import UDPConnectionError, UDPError, UDPResultsError
 from .hamburg import UDP
 
 __all__ = [
