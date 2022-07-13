9bffb35d8adca3856a10ae97f4a133ffd0b3e8aa
Klaas Schoute <klaas_schoute@hotmail.com>
2022-07-13 14:20:58 +0000

    Update test file

--- test_output.py
+++ test_output.py
@@ -9,10 +9,13 @@ from hamburg import UDPHamburg
 async def main() -> None:
     """Show example on using the Hamburg API client."""
     async with UDPHamburg() as client:
-        locations = await client.disabled_parkings(bulk="true")
+        disabled_parkings = await client.disabled_parkings(bulk="true")
+        print(disabled_parkings)
+
+        park_and_ride = await client.park_and_ride(bulk="true")
         count: int
 
-        for index, item in enumerate(locations, 1):
+        for index, item in enumerate(park_and_ride, 1):
             count = index
             print(item)
         print(f"{count} locations found")
